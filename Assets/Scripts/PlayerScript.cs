40aeaa2778ea200791e64b367aaf5c5e8a9f72a8
acoross <acoross@gmail.com>
2015-05-05 16:18:00 +0900

    walk animation (8 way)
    § TODO:
    □ 방향 전환이 더 자연스럽게 수정… (how?)
    □ Idle 상태 추가 후, 이동 키 누른 상태에서만 이동 애니메이션으로

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public enum Direction
@@ -6,7 +6,11 @@ public enum Direction
 	Down,
 	Up,
 	Left,
-	Right
+	Right,
+	UR,
+	UL,
+	DR,
+	DL
 }
 
 public class PlayerScript : MonoBehaviour 
@@ -28,34 +32,148 @@ public class PlayerScript : MonoBehaviour
 		
 		horizontal = (int) Input.GetAxisRaw ("Horizontal");
 		vertical = (int) Input.GetAxisRaw ("Vertical");
+
+		Move (horizontal, vertical);
+	}
+
+	private void Move(int horizontal, int vertical)
+	{
+		if (horizontal == 0)
+		{
+			if (vertical > 0)
+			{
+				TurnTo(Direction.Up);
+			}
+			else if (vertical < 0)
+			{
+				TurnTo (Direction.Down);
+			}
+		}
+		// 대각선 이동 - Right
+		else if (horizontal > 0)
+		{
+			// UR
+			if (vertical > 0)
+			{
+				TurnTo (Direction.UR);
+			}
+			// DR
+			else if (vertical < 0)
+			{
+				TurnTo (Direction.DR);
+			}
+			// right
+			else 
+			{
+				TurnTo (Direction.Right);
+			}
+		}
+		// 대각선 이동 - Left
+		else if (horizontal < 0)
+		{
+			// UL
+			if (vertical > 0)
+			{
+				TurnTo (Direction.UL);
+			}
+			// DL
+			else if (vertical < 0)
+			{
+				TurnTo (Direction.DL);
+			}
+			// left
+			else
+			{
+				TurnTo (Direction.Left);
+			}
+		}
+	}
+
+	#region coroutine test...
+	private IEnumerator CoTurnTo(Direction dir)
+	{
+		if (m_Direction == dir)
+			yield break;
 		
-		if (vertical != 0)
+		m_Direction = dir;
+
+		switch (dir)
 		{
-			horizontal = 0;
-			if (vertical > 0 && m_Direction != Direction.Up)
-			{
-				m_animator.SetTrigger("upTrigger");
-				m_Direction = Direction.Up;
-			}
-			else if (vertical < 0 && m_Direction != Direction.Down)
-			{
-				m_animator.SetTrigger("downTrigger");
-				m_Direction = Direction.Down;
-			}
+		case Direction.Up:
+			m_animator.SetTrigger("upTrigger");
+			yield return new WaitForSeconds(0.1f);
+			break;
+		case Direction.Down:
+			m_animator.SetTrigger("downTrigger");
+			yield return new WaitForSeconds(0.1f);
+			break;
+		case Direction.Right:
+			m_animator.SetTrigger("rightTrigger");
+			yield return new WaitForSeconds(0.1f);
+			break;
+		case Direction.Left:
+			m_animator.SetTrigger("leftTrigger");
+			yield return new WaitForSeconds(0.1f);
+			break;
+		case Direction.UR:
+			m_animator.SetTrigger("URTrigger");
+			yield return new WaitForSeconds(0.1f);
+			break;
+		case Direction.UL:
+			m_animator.SetTrigger("ULTrigger");
+			yield return new WaitForSeconds(0.1f);
+			break;
+		case Direction.DR:
+			m_animator.SetTrigger("DRTrigger");
+			yield return new WaitForSeconds(0.1f);
+			break;
+		case Direction.DL:
+			m_animator.SetTrigger("DLTrigger");
+			yield return new WaitForSeconds(0.1f);
+			break;
+		default:
+			break;
 		}
-		else if (horizontal != 0)
+
+		yield break;
+	}
+	#endregion
+
+	private void TurnTo(Direction dir)
+	{
+		if (m_Direction == dir)
+			return;
+
+		switch (dir)
 		{
-			vertical = 0;
-			if (horizontal > 0 && m_Direction != Direction.Right)
-			{
-				m_animator.SetTrigger("rightTrigger");
-				m_Direction = Direction.Right;
-			}
-			else if (horizontal < 0 && m_Direction != Direction.Left)
-			{
-				m_animator.SetTrigger("leftTrigger");
-				m_Direction = Direction.Left;
-			}
+		case Direction.Up:
+			m_animator.SetTrigger("upTrigger");
+			break;
+		case Direction.Down:
+			m_animator.SetTrigger("downTrigger");
+			break;
+		case Direction.Right:
+			m_animator.SetTrigger("rightTrigger");
+			break;
+		case Direction.Left:
+			m_animator.SetTrigger("leftTrigger");
+			break;
+		case Direction.UR:
+			m_animator.SetTrigger("URTrigger");
+			break;
+		case Direction.UL:
+			m_animator.SetTrigger("ULTrigger");
+			break;
+		case Direction.DR:
+			m_animator.SetTrigger("DRTrigger");
+			break;
+		case Direction.DL:
+			m_animator.SetTrigger("DLTrigger");
+			break;
+		default:
+			break;
 		}
+
+		m_Direction = dir;
 	}
 }
